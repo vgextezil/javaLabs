c3847b57db8d87e4ed27354fa2db944082ba035a
ExTeZil <vasiagakman.v.g.40@gmail.com>
2023-02-11 21:17:30 +0200

    trying lab3

---
+++ Lab3/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean check =false;
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введіть довжину масиву");
+            int massLenght = scanner.nextInt();
+
+        }
+        catch (InputMismatchException e){
+           
+        }
+
+    }
+}
