0789149da94a1ad068613773dc0622b2331156c3
ExTeZil <vasiagakman.v.g.40@gmail.com>
2023-02-23 00:12:31 +0200

    той самий комміт

--- Lab4/src/Dishes.java
+++ Lab4/src/Dishes.java
@@ -1,2 +1,67 @@
-package PACKAGE_NAME;public class Dishes {
+import java.time.LocalDate;
+
+public class Dishes{
+    private int id;
+    private String type;
+    private String title;
+
+    private float price;
+    private LocalDate expirationDate;
+
+    public Dishes(int id, String type, String title, float price,LocalDate expirationDate){
+        this.id = id;
+        this.type = type;
+        this.title = title;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return expirationDate;
+    }
+
+    public void setDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", Тип='" + type + '\'' +
+                ", Назва='" + title + '\'' +
+                ", Ціна=" + price +
+                ", Вжити До=" + expirationDate;
+    }
 }
--- Lab4/src/Workers.java
+++ Lab4/src/Workers.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Workers {
+import java.time.LocalDate;
+
+public class Workers extends Visitants{
+    private String pip;
+    private String position;
+    private LocalDate dateOfBirth;
+    public Workers(int id, String pip,String position, LocalDate dateOfBirth){
+        super(id);
+        this.pip = pip;
+        this.position= position;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Workers{" +
+                "id='" + getId() + '\'' +
+                "ПІП='" + pip + '\'' +
+                ", Посада='" + position + '\'' +
+                ", Дата народження=" + dateOfBirth +
+                '}';
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public void setId(int id) {
+        super.setId(id);
+    }
+
 }
